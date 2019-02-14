619dc1375ad0e5727e636334a25ceabea76c50dc
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-02-14 18:01:16 +0300

    template for toggle

--- projects/toggle-lib/src/lib/components/toggle/toggle.component.ts
+++ projects/toggle-lib/src/lib/components/toggle/toggle.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'next-toggle',
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.css']
+  styleUrls: ['./toggle.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToggleComponent implements OnInit {
+  @Input() disabled: boolean;
+  @Input() labelPosition: 'before' | 'after' = 'after';
+  @Input() required: boolean;
+  @Input() tabIndex: number;
 
   constructor() { }
 
