f62b52f6a89567eb796a21a2b9fbe3e716c1df30
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-02-21 13:47:25 +0300

    toggle style has been updated

--- projects/toggle-lib/src/public_api.ts
+++ projects/toggle-lib/src/public_api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of toggle-lib
  */
 
-export * from './lib/toggle-lib.service';
 export * from './lib/toggle-lib.component';
 export * from './lib/toggle-lib.module';
 export * from './lib/components/toggle/toggle.component';
