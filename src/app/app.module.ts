b5bf27d5d1907aef0b5f4e91ac56d239d65bdce3
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-02-22 15:13:30 +0300

    stories about toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ToggleLibModule } from 'toggle-lib';
+import { NextToggleModule } from 'toggle-lib';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -11,7 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    ToggleLibModule,
+    NextToggleModule,
     FormsModule,
     ReactiveFormsModule,
   ],
